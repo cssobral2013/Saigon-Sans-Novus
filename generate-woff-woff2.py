5f720b1e079beb9fe8a0a689c906eceb42dd3f1d
Cristiano Sobral <cssobral2013@gmail.com>
2025-11-05 00:27:11 -0300

    Update v6.0

---
+++ generate-woff-woff2.py
@@ -0,0 +1,329 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/SaigonSansNovus-Thin.ttf'
+woffPath = './web/SaigonSansNovus-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/SaigonSansNovus-ThinItalic.ttf'
+woffPath = './web/SaigonSansNovus-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/SaigonSansNovus-ExtraLight.ttf'
+woffPath = './web/SaigonSansNovus-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/SaigonSansNovus-ExtraLightItalic.ttf'
+woffPath = './web/SaigonSansNovus-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/SaigonSansNovus-Light.ttf'
+woffPath = './web/SaigonSansNovus-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-LightItalic.ttf'
+woffPath = './web/SaigonSansNovus-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-LightItalic.ttf'
+woffPath = './web/SaigonSansNovus-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-Regular.ttf'
+woffPath = './web/SaigonSansNovus-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-Italic.ttf'
+woffPath = './web/SaigonSansNovus-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-Medium.ttf'
+woffPath = './web/SaigonSansNovus-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-MediumItalic.ttf'
+woffPath = './web/SaigonSansNovus-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-SemiBold.ttf'
+woffPath = './web/SaigonSansNovus-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-SemiBoldItalic.ttf'
+woffPath = './web/SaigonSansNovus-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-SemiBold.ttf'
+woffPath = './web/SaigonSansNovus-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-SemiBoldItalic.ttf'
+woffPath = './web/SaigonSansNovus-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-Bold.ttf'
+woffPath = './web/SaigonSansNovus-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-BoldItalic.ttf'
+woffPath = './web/SaigonSansNovus-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-ExtraBold.ttf'
+woffPath = './web/SaigonSansNovus-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-ExtraBoldItalic.ttf'
+woffPath = './web/SaigonSansNovus-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/SaigonSansNovus-Black.ttf'
+woffPath = './web/SaigonSansNovus-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+ 
+ 
+srcPath  = './ttf/SaigonSansNovus-BlackItalic.ttf'
+woffPath = './web/SaigonSansNovus-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/SaigonSansNovus-Thin.ttf'
+woff2Path = './web/SaigonSansNovus-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/SaigonSansNovus-ThinItalic.ttf'
+woff2Path = './web/SaigonSansNovus-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/SaigonSansNovus-ExtraLight.ttf'
+woff2Path = './web/SaigonSansNovus-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/SaigonSansNovus-ExtraLightItalic.ttf'
+woff2Path = './web/SaigonSansNovus-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/SaigonSansNovus-Light.ttf'
+woff2Path = './web/SaigonSansNovus-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-LightItalic.ttf'
+woff2Path = './web/SaigonSansNovus-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-LightItalic.ttf'
+woff2Path = './web/SaigonSansNovus-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-Regular.ttf'
+woff2Path = './web/SaigonSansNovus-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-Italic.ttf'
+woff2Path = './web/SaigonSansNovus-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-Medium.ttf'
+woff2Path = './web/SaigonSansNovus-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-MediumItalic.ttf'
+woff2Path = './web/SaigonSansNovus-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-SemiBold.ttf'
+woff2Path = './web/SaigonSansNovus-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-SemiBoldItalic.ttf'
+woff2Path = './web/SaigonSansNovus-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-SemiBold.ttf'
+woff2Path = './web/SaigonSansNovus-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-SemiBoldItalic.ttf'
+woff2Path = './web/SaigonSansNovus-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-Bold.ttf'
+woff2Path = './web/SaigonSansNovus-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-BoldItalic.ttf'
+woff2Path = './web/SaigonSansNovus-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/SaigonSansNovus-ExtraBold.ttf'
+woff2Path = './web/SaigonSansNovus-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+ 
+srcPath  = './ttf/SaigonSansNovus-ExtraBoldItalic.ttf'
+woff2Path = './web/SaigonSansNovus-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/SaigonSansNovus-Black.ttf'
+woff2Path = './web/SaigonSansNovus-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+ 
+srcPath  = './ttf/SaigonSansNovus-BlackItalic.ttf'
+woff2Path = './web/SaigonSansNovus-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
